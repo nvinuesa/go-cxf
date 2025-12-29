79e106394bb01b61081af0b461b8a495ac989b9e
Nicolas Vinuesa <vinuesa.nico@gmail.com>
2025-12-29 22:33:16 +0100

    feat(security): add size-limited CBOR decoding APIs

--- cbor.go
+++ cbor.go
@@ -2,6 +2,7 @@ package cxf
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/fxamacker/cbor/v2"
 )
@@ -52,6 +53,16 @@ func UnmarshalHeaderCBOR(data []byte) (*Header, error) {
 	return &header, nil
 }
 
+// UnmarshalHeaderCBORSized deserializes CBOR into a Header while enforcing an input size limit.
+// This is intended for use with untrusted inputs to mitigate memory/DoS risks.
+func UnmarshalHeaderCBORSized(r io.Reader, maxBytes int64) (*Header, error) {
+	var header Header
+	if err := DecodeCBORSized(r, maxBytes, &header); err != nil {
+		return nil, err
+	}
+	return &header, nil
+}
+
 // EncodeCBOR encodes arbitrary data to CBOR format.
 func EncodeCBOR(v interface{}) ([]byte, error) {
 	return cborEncMode.Marshal(v)
@@ -60,4 +71,31 @@ func EncodeCBOR(v interface{}) ([]byte, error) {
 // DecodeCBOR decodes CBOR data into the provided value.
 func DecodeCBOR(data []byte, v interface{}) error {
 	return cborDecMode.Unmarshal(data, v)
+}
+
+// DecodeCBORSized decodes CBOR from an io.Reader, enforcing a hard input size limit and strict
+// single-document framing (no trailing non-whitespace).
+//
+// This is intended for use with untrusted inputs.
+func DecodeCBORSized(r io.Reader, maxBytes int64, v interface{}) error {
+	if maxBytes <= 0 {
+		return fmt.Errorf("maxBytes must be positive")
+	}
+
+	dec := cborDecMode.NewDecoder(io.LimitReader(r, maxBytes))
+	if err := dec.Decode(v); err != nil {
+		return fmt.Errorf("failed to decode CBOR: %w", err)
+	}
+
+	// Enforce "exactly one CBOR data item" semantics: after decoding v, the stream must be EOF.
+	// (CBOR doesn't have whitespace, so any extra bytes are trailing data.)
+	var extra interface{}
+	if err := dec.Decode(&extra); err != io.EOF {
+		if err == nil {
+			return fmt.Errorf("unexpected trailing data after CBOR item")
+		}
+		return fmt.Errorf("failed while checking trailing CBOR data: %w", err)
+	}
+
+	return nil
 }
